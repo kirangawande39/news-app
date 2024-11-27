27d96806e7af422011608193c68d72227761f901
kiran <kirangawande9307@gmail.com>
2024-11-27 22:08:30 +0530

    update a news.js file

--- frontend/src/pages/News.js
+++ frontend/src/pages/News.js
@@ -1,67 +1,80 @@
-import React, { useEffect, useState } from 'react'
-import NewsItems from './NewsItems'
-import Spinnar from './Spinnar'
-import '../App'
-// import Data from '../Data'
+import React, { useEffect, useState } from 'react';
+import NewsItems from './NewsItems';
+import Spinnar from './Spinnar';
 
 export default function News(props) {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [page, setPage] = useState(1);
+    const [totalResults, setTotalResults] = useState(0);
 
-    let [data, setData] = useState([])
-    let [loading, setLoading] = useState()
-    let [page, setPage] = useState(1)
-    let [totalResults,settResutl]=useState()
-    
+    const fetchData = async (pageNumber) => {
+        try {
+            props.setProgress(40);
+            const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=5394c3d0131942a8b6d56d400563313f&page=${pageNumber}&pageSize=${props.pageSize}`;
+            setLoading(true);
 
-    const fetchData = async () => {
-        props.setProgress(40)
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=5394c3d0131942a8b6d56d400563313f&page=${page}&pageSize=${props.pageSize}`;
-        setLoading(true)
+            const response = await fetch(url);
+            const parsedData = await response.json();
 
-        const response = await fetch(url);
-        const parsedData = await response.json();
+            props.setProgress(70);
 
-        props.setProgress(70)
-        setLoading(false)
-        
-        setData(parsedData.articles)
-        settResutl(parsedData.totalResults)
-        props.setProgress(100)
- 
-    }
-    
+            setData(parsedData.articles || []);
+            setTotalResults(parsedData.totalResults || 0);
+            setLoading(false);
+
+            props.setProgress(100);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            setLoading(false);
+        }
+    };
+
     useEffect(() => {
-        fetchData();
-    }, []);
+        fetchData(page);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [page]);
 
-    const handleNextBtn =async () => {
-        setPage(page + 1)
-        fetchData();
-    }
+    const handleNextBtn = () => {
+        if (page + 1 <= Math.ceil(totalResults / props.pageSize)) {
+            setPage(page + 1);
+        }
+    };
 
-    const handlePrivesBtn =async () => {
-        setPage(page - 1)
-        fetchData();
-    }
-
-
-
+    const handlePrevBtn = () => {
+        if (page > 1) {
+            setPage(page - 1);
+        }
+    };
 
     return (
-        <div className='container ' style={{marginTop:'100px'}}>
-
-            <h1 className='text-center'>Kal-Tak {props.catagery}</h1>
+        <div className="container" style={{ marginTop: '100px' }}>
+            <h1 className="text-center">Kal-Tak {props.category}</h1>
             {loading && <Spinnar />}
-            <div className='row'>
-                {data.map((item, i) => {
-                    return <div className='col-md-4 my-3' key={i}>
-                        <NewsItems element={item} />
-                    </div>
-                })}
+            <div className="row">
+                {!loading &&
+                    data.map((item, i) => (
+                        <div className="col-md-4 my-3" key={i}>
+                            <NewsItems element={item} />
+                        </div>
+                    ))}
             </div>
-            <div className='container d-flex justify-content-between'>
-                <button disabled={page <= 1} type="button" onClick={handlePrivesBtn} className="btn btn-primary">&larr;Prives</button>
-                <button disabled={page + 1 > Math.ceil(totalResults / props.pageSize)}  type="button" onClick={handleNextBtn} className="btn btn-primary">Next&rarr;</button>
+            <div className="container d-flex justify-content-between my-3">
+                <button
+                    disabled={page <= 1}
+                    onClick={handlePrevBtn}
+                    className="btn btn-primary"
+                >
+                    &larr; Previous
+                </button>
+                <button
+                    disabled={page + 1 > Math.ceil(totalResults / props.pageSize)}
+                    onClick={handleNextBtn}
+                    className="btn btn-primary"
+                >
+                    Next &rarr;
+                </button>
             </div>
         </div>
-    )
+    );
 }
